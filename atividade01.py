64238ca8f04635d3c10404e393468c5f55ebcfa9
Hugo Sales <hugosalespe@gmail.com>
2025-07-05 16:26:00 -0300

    Add files via upload

---
+++ atividade01.py
@@ -0,0 +1,4 @@
+mensagem = 'Eu amo programar em Python'
+print(mensagem)
+print(mensagem.upper()) # Caixa Alta
+print(mensagem.lower()) #Caixa Baixa
---
+++ atividade04.py
@@ -0,0 +1,11 @@
+#Variáveis
+celsius = float(input('Informe o valor da temperatura: '))
+
+#Funções e Métodos
+
+#Processamento
+fahrenheit = (celsius * 1.8 + 32)
+
+#Visualização
+print(f'Temperatura em Fahrenheit: {fahrenheit:.2f}')
+
