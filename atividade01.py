9f14d549207cc0279847f8803fbab1cb7dde8597
Hugo Sales <hugosalespe@gmail.com>
2025-07-19 10:28:48 -0300

    Atualização Aula 02

--- atividade01.py
+++
@@ -1,4 +0,0 @@
-mensagem = 'Eu amo programar em Python'
-print(mensagem)
-print(mensagem.upper()) # Caixa Alta
-print(mensagem.lower()) #Caixa Baixa
---
+++ aula_01/01_olamundo.py
@@ -0,0 +1,14 @@
+print('Olá, mundo!') # String
+print(11) # Inteiro (int)
+print(True) # Boleano True False
+print(11.07) # Float
+
+print('---- x ----- x ---- x ----')
+
+#Variável
+nome = 'Hugo'
+print(nome)
+
+#Input
+idade = input('Digite sua idade: ')
+print(idade)
